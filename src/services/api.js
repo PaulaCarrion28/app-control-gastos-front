4f4120da4046565c8a1f439848509de8385ce990
Paula Carrion <paula-carrion28@hotmail.com>
2026-03-18 08:27:01 -0500

    (chore): definición de endpoints y urls base de la api para próxima comunicación con el back

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+const URL_BASE = "localhost:8080/"
+
+export let end_points = {
+    users: URL_BASE + "users/",
+    merchants: URL_BASE + "merchants/",
+    expenses: URL_BASE + "expenses/",
+    payment_methos : URBL_BASE + "payment_methods/",
+    category: URL_BASE + "category/"
+
+}
